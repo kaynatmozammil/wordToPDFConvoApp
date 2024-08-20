0598692216963e5a5db0a96fcdcbb3f5200e462d
kaynatmozammil <kaynatmozammil@gmail.com>
2024-08-21 04:36:14 +0530

    Done!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Home from './components/home'
+import Footer from './components/Footer'
+export default function App() {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <>
+            <div className='items-center justify-centere'>
+                <hr className=' border-black' />
+                <h1 className='text-center py-3 text-sm '>Copyright &copy; 2024 - All right reserved by ACME Industries Ltd</h1>
+            </div>
+        </>
+    )
+}
